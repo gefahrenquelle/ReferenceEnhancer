b6fda701a1385a652695c8e13adf0af20f5465f5
Benjamin Redl <benjamin.redl.99@gmail.com>
2025-05-06 11:38:56 +0200

    only substract -1 of end of exon if end > start in order for increased flexibility of input .gtfs and potential 1 base sequences

--- R/exon_overlap.R
+++ R/exon_overlap.R
@@ -1,5 +1,5 @@
 exon_overlap <- function(gene_A_exons, gene_B_exons){
-
+  
   if(dim(gene_A_exons)[1] == 0 | dim(gene_B_exons)[1] == 0){
     return (FALSE)
   }
@@ -7,8 +7,24 @@ exon_overlap <- function(gene_A_exons, gene_B_exons){
   for(row_exonA in 1:nrow(gene_A_exons)){
     for(row_exonB in 1:nrow(gene_B_exons)){
 
-      x = seq(from = gene_A_exons[row_exonA,1], to = gene_A_exons[row_exonA,2]-1, by = 1)
-      y = seq(from = gene_B_exons[row_exonB,1], to = gene_B_exons[row_exonB,2]-1, by = 1)
+      if (any(gene_A_exons[,1] <= 0 | gene_A_exons[,2] <= 0) | 
+        any(gene_B_exons[,1] <= 0 | gene_B_exons[,2] <= 0)) {
+        return(TRUE)  
+      }
+
+      from_x <- gene_A_exons[row_exonA,1]
+      to_x <- gene_A_exons[row_exonA,2]
+      if (to_x > from_x) {
+        to_x <- to_x - 1
+      }
+      x = seq(from = from_x, to = to_x, by = 1)
+
+      from_y <- gene_B_exons[row_exonB,1]
+      to_y <- gene_B_exons[row_exonB,2]
+      if (to_y > from_y) {
+        to_y <- to_y - 1
+      }
+      y = seq(from = from_y, to = to_y, by = 1)
 
       if(length(intersect(x,y))!=0){
         return (TRUE)
